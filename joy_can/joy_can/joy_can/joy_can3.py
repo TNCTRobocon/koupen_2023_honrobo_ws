748f8454c1be161d3cae91e3d212e640a38c33b7
kou-pen <kou-pen@outlook.jp>
2023-09-04 13:08:45 +0900

    fix joy_axis

--- joy_can/joy_can/joy_can/joy_can3.py
+++ joy_can/joy_can/joy_can/joy_can3.py
@@ -53,7 +53,7 @@ class JoyCan(Node):
             ###F310
             recaluculated_joy[0] = joy.axes[0] * 127 + 128 #left-horizontal
             recaluculated_joy[1] = joy.axes[1] * 127 + 128 #left-vertical
-            recaluculated_joy[2] = joy.axes[3] * 127 + 128 #right-horizontal
+            recaluculated_joy[2] = joy.axes[3] * -127 + 128 #right-horizontal
             recaluculated_joy[3] = joy.axes[4] * 127 + 128 #right-vertical
             recaluculated_joy[4] = joy.axes[2] * -127 + 128
             recaluculated_joy[5] = joy.axes[5] * -127 + 128
