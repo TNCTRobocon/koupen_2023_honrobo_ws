39870aebc37d976bb38ab0cd639939f191f8000b
kou-pen <kou-pen@outlook.jp>
2023-10-05 01:22:06 +0900

    clean

--- auto_move/src/source_file.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source ~/HONROBO_WS/realsense_pub/src/install/setup.bash
-source ~/HONROBO_WS/ros_joy/install/setup.bash
-source ~/HONROBO_WS/auto_move/src/install/setup.bash
--- ros_joy/build/ros_joy/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/kohki/HONROBO_WS/ros_joy/install/ros_joy'
