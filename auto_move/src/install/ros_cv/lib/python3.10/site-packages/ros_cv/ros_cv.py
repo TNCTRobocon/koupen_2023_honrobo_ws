b6316681eff4976334e56848b01b7d2feb8899b5
kou-pen <kou-pen@outlook.jp>
2023-10-05 01:17:27 +0900

    add detection skipping

--- auto_move/src/install/ros_cv/lib/python3.10/site-packages/ros_cv/ros_cv.py
+++ auto_move/src/install/ros_cv/lib/python3.10/site-packages/ros_cv/ros_cv.py
@@ -2,7 +2,6 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image,Joy,CompressedImage
 from cv_bridge import CvBridge
-from rclpy.qos import qos_profile_sensor_data
 
 from ultralytics import YOLO
 import cv2
@@ -12,22 +11,26 @@ import numpy as np
 from .module.JoyCalcTools import JoyCalcTools
 
 class RosCv(Node):
+    global joy_data_r,cv_image
     node_name = 'ros_cv'
     sub_joy_topic_name = 'joy'
-    # sub_img_topic_name = 'image_topic_compressed'
+    # sub_img_topic_name = 'image_topic_compressed' # 圧縮時のトピック名
     sub_img_topic_name = "image_topic"
     
     pub_joy_topic_name = 'converted_joy'
     pub_img_topic_name = 'image_result'
-    axis_max = 50
+    axis_max = 50 # どこまで操作するかパラメータ
+    arrow_fruits_lost = 30 # 何フレーム検出飛びを許すか
+    none_detect_count = 0 # 記録用変数
+    
 
     
     def __init__(self):
         global joy_data_r,cv_image
         super().__init__(self.node_name)
         
-        self.sub = self.create_subscription(Joy,self.sub_joy_topic_name,self.callback,10)
-        self.sub2 = self.create_subscription(Image,self.sub_img_topic_name,self.callback2,10)
+        self.sub = self.create_subscription(Joy,self.sub_joy_topic_name,self.joy_callback,10)
+        self.sub2 = self.create_subscription(Image,self.sub_img_topic_name,self.image_callback,10)
         
         self.pub = self.create_publisher(Joy,self.pub_joy_topic_name,10)
         self.pub2 = self.create_publisher(Image,self.pub_img_topic_name,10)
@@ -37,37 +40,40 @@ class RosCv(Node):
 
         self.model = YOLO("/home/kohki/HONROBO_WS/auto_move/src/ros_cv/ros_cv/weights/best.pt")
         self.joy_tool = JoyCalcTools(1)
-        self.get_logger().info("auto_move init")
         
-    def callback(self,data):
+    def joy_callback(self,data):
         global joy_data_r
         joy_data_r = data
         
-    def callback2(self,image):
+    def image_callback(self,image):
         global cv_image
         
+        # 圧縮あり1
         # self.np_arr = np.frombuffer(image.data, np.uint8)
         # cv_image = cv2.imdecode(self.np_arr, cv2.IMREAD_COLOR)
         
-        cv_image = self.bridge.imgmsg_to_cv2(image)
-        
+        # 圧縮あり2
         # cv_image = self.bridge.compressed_imgmsg_to_cv2(image)
-        self.get_logger().info("callbacked2")
+        
+        # 圧縮しない
+        cv_image = self.bridge.imgmsg_to_cv2(image)
         self.mainprocess()
         
     def mainprocess(self):
         global joy_data_r,cv_image
-        self.get_logger().info("callbacked main")
-        frame, results = self.detect_fruits(cv_image)
-        dis, per, pm = self.calc_points(frame,results)
-        ans = self.calc_move(dis,per)
-        recalc_joy = self.override_side_joy(joy_data_r,ans,pm)
-        self.publish_joy(recalc_joy,frame)
+        try: #Joyデータを受信していなかった際に待機(処理を飛ばす)
+            if joy_data_r:
+                frame, results = self.detect_fruits(cv_image)
+                dis, per, pm = self.calc_points(frame,results)
+                ans = self.calc_move(dis,per)
+                recalc_joy = self.override_side_joy(joy_data_r,ans,pm)
+                self.publish_joy(recalc_joy,frame)
+        except:
+            pass
         
 
     def detect_fruits(self,img):
-        # ret, frame = self.cap.read()
-        results = self.model.track(source=img, tracker="botsort.yaml", conf=0.8, iou=0.8)
+        results = self.model.track(source=img, tracker="botsort.yaml", conf=0.5, iou=0.5, persist=True) # 検出&トラッキング
         return img,results
     
     def calc_points(self,frame,results):
@@ -77,40 +83,50 @@ class RosCv(Node):
         self.frame_y, self.frame_x, _ = frame.shape
         self.frame_centor_x = int(self.frame_x * 0.5)
         self.frame_centor_y = int(self.frame_y * 0.5)
-        # self.distance_max = math.sqrt((self.frame_centor_x ** 2) + (frame_centor_y ** 2))
         self.distance_max = self.frame_centor_x
-        cv2.line(frame, (self.frame_centor_x, 0), (self.frame_centor_x, self.frame_y),(0, 255, 0),thickness=2)
+        
+        cv2.line(frame, (self.frame_centor_x, 0), (self.frame_centor_x, self.frame_y),(0, 255, 0),thickness=2) # 中心線
         if bbox_np.size:
+            self.none_detect_count = 0
             x1 = int(bbox_np[0][0])
             y1 = int(bbox_np[0][1])
             x2 = int(bbox_np[0][2])
             y2 = int(bbox_np[0][3])
             centor_x = int((x1 + x2) * 0.5)
             centor_y = int((y1 + y2) * 0.5)
+            helf_x1 = int((x1 + centor_x)*0.5)
+            helf_x2 = int((x2 + centor_x)*0.5)
             
             cv2.rectangle(frame, (x1,y1), (x2,y2), (0,255,0))
-            cv2.drawMarker(frame, (centor_x, centor_y), color=(0, 255, 0), markerType=cv2.MARKER_CROSS, markerSize=4, thickness=2)   
+            cv2.rectangle(frame, (helf_x1,y1), (helf_x2,y2), (0,255,0)) 
             cv2.line(frame, (self.frame_centor_x, centor_y), (centor_x, centor_y),(0, 255, 0),thickness=2)
             
+            self.distance_of_two = abs(self.frame_centor_x - centor_x)
+            self.per_of_frame = ((x2-x1) * (y2-y1)) / (self.frame_x * self.frame_y)
             
-            # distance_of_two = math.sqrt((self.frame_centor_x - centor_x) ** 2 + (frame_centor_y - centor_y) ** 2)
-            distance_of_two = abs(self.frame_centor_x - centor_x)
-            per_of_frame = ((x2-x1) * (y2-y1)) / (self.frame_x * self.frame_y)
-            
-            # pm = -1 if centor_x < self.frame_centor_x else 1
-            if centor_x < self.frame_centor_x:
-                pm = -1
-            else :
-                pm = 1
-            
-            # cv2.imshow("video",frame)
-            # cv2.waitKey(1)
 
-            return distance_of_two, per_of_frame, pm
+            if centor_x < self.frame_centor_x:
+                self.direction = -1
+            else :
+                self.direction = 1
+
+            print(self.none_detect_count)
+            
+            if helf_x1 < self.frame_centor_x and self.frame_centor_x < helf_x2:
+                return 0, 0, 0
+            return self.distance_of_two, self.per_of_frame, self.direction
         
-        # cv2.imshow("video",frame)
-        # cv2.waitKey(1)
-        return 0, 0, 0
+        if self.none_detect_count > self.arrow_fruits_lost:
+            self.none_detect_count += 1
+            print(self.none_detect_count)
+            return 0, 0, 0
+        self.none_detect_count += 1
+        try:
+            print(self.none_detect_count)
+            return self.distance_of_two, self.per_of_frame, self.direction
+        except:
+            print(self.none_detect_count)
+            return 0, 0, 0
     
     def calc_move(self,distance,percentage):
         distance_percent = distance / self.distance_max
@@ -118,14 +134,13 @@ class RosCv(Node):
         return distance_percent * percentage 
 
     
-    def override_side_joy(self,joy,ans,pm):
+    def override_side_joy(self,joy,ans,direc):
         target_joy = joy
-        diff = ans * self.axis_max * pm / 127
+        diff = ans * self.axis_max * direc / 127
         clamp= lambda x: min(1, max(x, -1))
         diff = target_joy.axes[2] + diff
         
         target_joy.axes[2] = clamp(diff)
-        print(target_joy.axes[2])
         return target_joy
     
     def publish_joy(self,joy,frame):
