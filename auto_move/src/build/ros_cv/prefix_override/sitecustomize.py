e16e558c6754a5cc4ba7d69362c42d7bb568e498
kou-pen <kou-pen@outlook.jp>
2023-10-05 01:31:41 +0900

    delete tmp_file

--- auto_move/src/build/ros_cv/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/kohki/HONROBO_WS/auto_move/src/install/ros_cv'
--- can_messenger/src/build/can_messenger/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/kohki/HONROBO_WS/can_messenger/src/install/can_messenger'
